e71acc9943f5d02af148538239765875bd63361f
Bruno de Andrade Dinis <bruno.a.dinis96@gmail.com>
2025-05-29 23:42:24 -0300

    feat: Implement session management and redirection in authentication and dashboard pages

--- src/app/authentication/page.tsx
+++ src/app/authentication/page.tsx
@@ -1,9 +1,20 @@
-"use client";
-
+import { redirect } from "next/navigation";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import SignUpForm from "./components/sign-up-form";
 import LoginForm from "./components/login+form";
-const AuthenticationPage = () => {
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+
+
+const AuthenticationPage = async() => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if(session?.user){
+    redirect('/dashboard')
+  }
+  
   return (
     <div className="h-screen w-screen flex items-center justify-center">
       <Tabs defaultValue="login" className="w-[400px]">
